c49fa9e57bd7ab8848e74ce098c37a68479a3a0b
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-06-17 07:32:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -80,7 +80,7 @@ async def Jisshu_start():
     today = date.today()
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
-    await JisshuBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+ #   await JisshuBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
