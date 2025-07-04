b06043982ea26ed4389a26708b8f26632943e804
suryadeeprampur <suryadeep.rampur@gmail.com>
2025-07-04 13:22:15 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -114,7 +114,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """{}"""
+    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「......」</b>"""
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
